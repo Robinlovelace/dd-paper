940576f8c6439850612bd33b71dc21a8d512c48f
Robin Lovelace <rob00x@gmail.com>
2021-01-12 21:20:18 +0000

    Update with new graphics

--- code/decay-funs.R
+++ code/decay-funs.R
@@ -1,4 +1,4 @@
-remotes::install_cran("minpack.lm")
+# remotes::install_cran("minpack.lm")
 library(tidyverse)
 
 # l = pct::get_pct(geography = "msoa", layer = "l", national = TRUE)
@@ -10,9 +10,10 @@ regions = c("cambridgeshire", "humberside", "west-yorkshire")
 modes_of_interest = c("foot", "bicycle", "car_driver")
 modes_of_interest = c("foot", "bicycle")
 
-l = purrr::map_dfr(regions, ~pct::get_pct_lines(., geography = "msoa"), .id = "id")
-sf::st_crs(l) = 4326
-saveRDS(l, "l_regions-2020-10.Rds")
+# l = purrr::map_dfr(regions, ~pct::get_pct_lines(., geography = "msoa"), .id = "id")
+# sf::st_crs(l) = 4326
+# saveRDS(l, "l_regions-2020-10.Rds")
+l = readRDS("l_regions-2020-10.Rds")
 names(l)
 # l_cities = l_cities %>% filter(all >= 20) %>%
 l_cities = l %>% filter(all >= 50) %>%
@@ -28,9 +29,9 @@ saveRDS(l_cities, "l_cities.Rds")
 l_cities_long = l_cities %>%
   select(-matches("pc|pw|pd")) %>%
   sf::st_drop_geometry() %>%
-  tidyr::pivot_longer(cols = bicycle:taxi_other, names_to = "Mode") %>%
+  tidyr::pivot_longer(cols = bicycle:taxi_other, names_to = "mode") %>%
   mutate(proportion = value/all) %>%
-  filter(Mode %in% modes_of_interest)
+  filter(mode %in% modes_of_interest)
 class(l_cities_long$region) # char
 l_cities_long$region = as.factor(l_cities_long$region)
 summary(l_cities_long$region)
@@ -40,75 +41,53 @@ levels(l_cities_long$region) = region_names_title
 
 ggplot(l_cities_long) +
   geom_point(aes(distance, proportion, size = all), alpha = 0.1) +
-  facet_grid(region ~ Mode) +
+  facet_grid(mode ~ region) +
   ylim(c(0, 1))
 
 # linear models
 
-by_city = l_cities %>%
-  sf::st_drop_geometry() %>%
-  group_by(region) %>%
+by_city = l_cities_long %>%
+  group_by(region, mode) %>%
   nest()
 
-
-
-modlm = function(df) {
-  # minpack.lm::nlsLM(pdrive ~ a1*dbeta(distance/max(distance), s1, s2), start = s_3, data = df, lower = c(0.1, 0.1, 0.1))
-  stats::lm(pdrive ~ distance, weights = all)
+modlm = function(d) {
+  stats::lm(proportion ~ distance, weights = all, data = d)
 }
 
-by_city_slm = by_city %>%
-  mutate(model_walk_lm = map(data, modlm), predw = map(modelw, predict),
-         modelc = map(data, modc), predc = map(modelc, predict),
-         modeld = map(data, modd), predd = map(modeld, predict)
-         ) %>%
+by_city_lm = by_city %>%
+  mutate(model_lm = map(data, modlm), pred = map(model_lm, predict)) %>%
+  unnest(cols = c(data, pred)) %>%
+  select(region, mode, all, distance, distance_euclidean, gradient, value, proportion, pred) %>%
+  ungroup()
 
+names(by_city_lm)
 
-by_city = by_city %>%
-  mutate(modelw = map(data, modw), predw = map(modelw, predict),
-         modelc = map(data, modc), predc = map(modelc, predict),
-         modeld = map(data, modd), predd = map(modeld, predict)
-         )
-# # saving as it doesn't work with knitr for some reason...
-# saveRDS(by_city, "by_city-beta.Rds")
-by_city = readRDS("by_city-beta.Rds")
-l_beta = unnest(by_city, cols = c(data, predw, predc, predd))
-# text_betaw = map_dfr(by_city$modelw, ~round(coef(.), digits = 3))
-# text_betaw$a1 = paste("a: ", text_betaw$a1)
-# text_betaw$s1 = paste("s1: ", text_betaw$s1)
-# text_betaw$s2 = paste("s2: ", text_betaw$s2)
-# text_betaw$region = region_names_title
-# text_betac = map_dfr(by_city$modelc, ~round(coef(.), digits = 3))
-# text_betac$a1 = paste("a: ", text_betac$a1)
-# text_betac$s1 = paste("s1: ", text_betac$s1)
-# text_betac$s2 = paste("s2: ", text_betac$s2)
-# text_betac$region = region_names_title
-
-# for printing model results
-
-g1 = ggplot(l_beta) +
-  geom_point(aes(distance, pwalk, size = all), alpha = 0.1) +
-  geom_line(aes(distance, predw), col = "red", size = 2) +
-  # geom_text(aes(x = 10, y = 0.8, label = a1), data = text_betaw) +
-  # geom_text(aes(x = 10, y = 0.7, label = s1), data = text_betaw) +
-  # geom_text(aes(x = 10, y = 0.6, label = s2), data = text_betaw) +
-  facet_wrap(~region) +
+g_lm = ggplot(by_city_lm) +
+  geom_point(aes(distance, proportion, size = all), alpha = 0.1) +
+  facet_grid(mode ~ region) +
+  geom_line(aes(distance, pred), col = "red", size = 2) +
   ylim(c(0, 1)) +
-  xlim(c(0, 5))
-g2 = ggplot(l_beta) +
-  geom_point(aes(distance, pcycle, size = all), alpha = 0.1) +
-  geom_line(aes(distance, predc), col = "red", size = 2) +
-  # geom_text(aes(x = 10, y = 0.8, label = a1), data = text_betac) +
-  # geom_text(aes(x = 10, y = 0.7, label = s1), data = text_betac) +
-  # geom_text(aes(x = 10, y = 0.6, label = s2), data = text_betac) +
-  facet_wrap(~region) +
-  ylim(c(0, 1))
-g3 = ggplot(l_beta) +
-  geom_point(aes(distance, pdrive, size = all), alpha = 0.1) +
-  geom_line(aes(distance, predd), col = "red", size = 2) +
-  # geom_text(aes(x = 10, y = 0.8, label = a1), data = text_betac) +
-  # geom_text(aes(x = 10, y = 0.7, label = s1), data = text_betac) +
-  # geom_text(aes(x = 10, y = 0.6, label = s2), data = text_betac) +
-  facet_wrap(~region) +
-  ylim(c(0, 1))
-g1 / g2 / g3
+  xlim(c(0, 10))
+
+saveRDS(by_city_lm, "by_city_lm.Rds" )
+
+s_2 = list(a1 = 0.1, s1 = 2, s2 = 3)
+s_3 = list(a1 = 0.5, s1 = 3, s2 = 1)
+modbeta = function(d) {
+  minpack.lm::nlsLM(proportion ~ a1*dbeta(distance/max(distance), s1, s2), start = s_2, data = d, weights = all)
+}
+
+by_city_beta = by_city %>%
+  mutate(mo = map(data, modbeta), pred = map(mo, predict)) %>%
+  unnest(cols = c(data, pred)) %>%
+  select(region, mode, all, distance, distance_euclidean, gradient, value, proportion, pred) %>%
+  ungroup()
+
+ggplot(by_city_beta) +
+  geom_point(aes(distance, proportion, size = all), alpha = 0.1) +
+  facet_grid(mode ~ region) +
+  geom_line(aes(distance, pred), col = "red", size = 2) +
+  ylim(c(0, 1)) +
+  xlim(c(0, 10))
+
+saveRDS(by_city_beta, "by_city_beta.Rds")
